2e7c76edd06bc366db42be7e325d39734cbbd679
Siyana Hristova <siyah.hristova@gmail.com>
2021-05-22 18:36:19 +0300

    Add files via upload

---
+++ barcodeScanner/dataProcessing.h
@@ -0,0 +1,6 @@
+#include "Defines.h"
+
+int* dataProcessing(float *arr, int numberOfSensors, int counter);
+char* castToChar(int *barCode, int counter);
+char* changeTheValue(char *charBarCode, int counter);
+char* reverseString(char* str);
