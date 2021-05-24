86dc923fbccae916189f71be1ee3fe91c90d8331
Dimitar Velev <76786770+DIVelev@users.noreply.github.com>
2021-05-24 20:43:26 +0300

    Add files via upload

---
+++ barcodeScannerFinal/include/dataProcessing.h
@@ -0,0 +1,5 @@
+#include "Defines.h"
+
+int* dataProcessing(float *arr, int numberOfSensors, int counter);
+char* castToChar(int *barCode, int counter);
+char* changeTheValue(char *charBarCode, int counter);
