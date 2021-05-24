6769411467f0c161152da440a654b9fc00a020b8
Dimitar Velev <76786770+DIVelev@users.noreply.github.com>
2021-05-24 20:44:21 +0300

    Delete barcodeScannerFinal directory

--- barcodeScannerFinal/include/dataProcessing.h
+++
@@ -1,5 +0,0 @@
-#include "Defines.h"
-
-int* dataProcessing(float *arr, int numberOfSensors, int counter);
-char* castToChar(int *barCode, int counter);
-char* changeTheValue(char *charBarCode, int counter);
